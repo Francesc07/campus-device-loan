4ec267644def80352ba6bf97ec8698c39c73df5c
Francis Ikenna Okoye <148211690+Francesc07@users.noreply.github.com>
2025-10-21 12:45:28 +0000

    feat: update package configuration and register HTTP-triggered functions

---
+++ services/device-catalog/src/functionApp.ts
@@ -0,0 +1,6 @@
+// Explicitly register all HTTP-triggered functions
+import "./API/functions/create-device-http";
+import "./API/functions/get-device-by-id-http";
+import "./API/functions/list-devices-http";
+import "./API/functions/update-device-http";
+import "./API/functions/delete-device-http";
